d176e84097488006efb9de3d2543e49f62437dfb
eo-39 <olumesemicheal@gmail.com>
2020-11-06 20:26:25 -0500

    Task: Refractor for Travis

--- src/CsvReader/CsvReader.py
+++ src/CsvReader/CsvReader.py
@@ -3,23 +3,22 @@ import csv
 from src.Fileutilities.absolutepath import absolute_path
 
 
-def class_factory(class_name, dictionary):
-    return type(class_name, (object,), dictionary)
+#def class_factory(class_name, dictionary):
+    #return type(class_name, (object,), dictionary)
 
 
 class CsvReader:
     data = []
 
+    def __init__(self):
+        pass
+
     def __init__(self, filepath):
-        self.data = []
-        with open(absolute_path(filepath)) as text_data:
+        with open(filepath, 'r') as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
-        pass
-
-    def return_data_as_class(self, class_name):
-        objects = []
-        for row in self.data:
-            objects.append(class_factory(class_name, row))
-        return objects
+        if not self.data:
+            raise Exception('List is empty.')
+        else:
+            return self.data
