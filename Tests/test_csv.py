6f90a7da75ab819ab73fcb46ac360d30f475efbf
eo-39 <olumesemicheal@gmail.com>
2020-11-08 15:17:29 -0500

    Bug: Empty List # out to build passing

--- Tests/test_csv.py
+++ Tests/test_csv.py
@@ -10,25 +10,22 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_parser(self):
         self.assertIsInstance(self.datafile, CsvReader)
 
-  #  def test_file_parser(self):
+    def test_file_parser(self):
        filepath = './Tests/Data/Unit Test Addition.csv'
        file_data = self.datafile.csv(filepath)
 
 #        for row in file_data:
 #            print(row)
-#        file_data.clear()
+       file_data.clear()
 
     def test_empty_list(self):
        #Empty list
-       #filepath = './Tests/Data/Unit Test Empty.csv'
+       filepath = './Tests/Data/Unit Test Empty.csv'
        #Non-Empty List
 
-       filepath = './Tests/Data/Unit Test Addition.csv'
+       #filepath = './Tests/Data/Unit Test Addition.csv'
        file_data = self.datafile.csv(filepath)
-       # for row in file_data:
-       #     print(row)
        file_data.clear()
-
 
 if __name__ == '__main__':
     unittest.main()
